970b565f8ced6bb00b64e2a288b881467689d78b
Nathan Saunders <nathan_saunders@hotmail.co.uk>
2017-07-05 21:38:57 +0000

    Added devise gem, posts and styling to login

---
+++ journeyandjournal/app/controllers/posts_controller.rb
@@ -0,0 +1,25 @@
+class PostsController < ApplicationController
+	
+	def new
+		@post = Post.new
+	end
+	
+	def create
+	end
+	
+	def edit
+	end
+	
+	def update
+	end
+	
+	def show
+	end
+	
+	def index
+	end
+	
+	def destroy
+	end
+	
+end
---
+++ journeyandjournal/db/migrate/20170705192843_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :posts do |t|
+      t.integer :user_id
+      t.integer :category_id
+      t.text :summary
+      t.text :improvements
+      t.text :objectives
+      t.timestamps
+    end
+  end
+end
